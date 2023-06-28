7f375d9bda4cb459617fb4a0c35b19d1b2a009d9
DarksideEvil <sinister58450@gmail.com>
2023-06-28 11:50:11 +0500

    front-end

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import AppRouter from './router/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+
+
+
